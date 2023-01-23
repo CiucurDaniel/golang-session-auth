2b3a3e2a2fb37afa111f31f461ac80c24c000ee5
Ciucur Daniel <ciucur.daniel14@gmail.com>
2023-01-23 15:38:28 +0200

    Add chi, setup basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/CiucurDaniel/golang-session-auth
 
 go 1.16
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
 	fmt.Println("Session auth")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Homepage reached"))
+	})
+
+	log.Fatal(http.ListenAndServe(":3060", r))
 }
