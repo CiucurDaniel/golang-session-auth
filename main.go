9b111104aeeed5a7dbdb34eca42fec43a3f0f235
Ciucur Daniel <ciucur.daniel14@gmail.com>
2023-01-23 15:44:53 +0200

    Move handler to separate function

--- main.go
+++ main.go
@@ -9,15 +9,17 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
+func hompageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Homepage reached"))
+}
+
 func main() {
 	fmt.Println("Session auth")
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Homepage reached"))
-	})
+	r.Get("/", hompageHandler)
 
 	log.Fatal(http.ListenAndServe(":3060", r))
 }
