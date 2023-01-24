34d5daf15151a33687b4bbaa8d5fb9007ec24d13
Ciucur Daniel <ciucur.daniel14@gmail.com>
2023-01-24 23:11:45 +0200

    Add templates for index, login, register

--- main.go
+++ main.go
@@ -4,13 +4,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
 func hompageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Homepage reached"))
+
+	template := template.Must(template.ParseFiles("./templates/index.html"))
+
+	err := template.Execute(w, "home")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+}
+
+func signInGetHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func signInPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func signOutGetHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func signOutPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func privatePage(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
